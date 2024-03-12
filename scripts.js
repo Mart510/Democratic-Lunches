b89d5e06da8c2deb1ed7653e3d8b7718231c839b
Mart510 <martynandrewbillings@gmail.com>
2024-03-12 10:47:43 +0000

    table generated from GET request

---
+++ scripts.js
@@ -0,0 +1,96 @@
+// import block
+
+// baseURL localhost for now can be changed later (dev or live switch for env ready)
+const baseURL = 'http://localhost:3000';
+
+// API funcs block
+
+// GET all poll options
+async function pollOptions() {
+    try {
+        // await fetch
+        const response = await fetch(`${baseURL}/options`);
+        // Parse json
+        const data = await response.json();
+        // debug logger
+        // console.log(data);
+        return data;
+        } catch (error) {
+            console.error('Failed to fetch');
+        }
+};
+
+
+// DOM manipulation block
+
+// build table in form to show the choices and results
+function lunchOptiosnTableConstructor(optionsList) {
+    // select the div
+    const voteAndResultsHolder = document.getElementById('voteAndResultsHolder');
+
+    // Create a table
+    const table = document.createElement('table');
+
+    // set table headers
+    const tableHeader = document.createElement('thead');
+    const headerRow = document.createElement('tr');
+    const optionNameHeader = document.createElement('th');
+    optionNameHeader.textContent = 'Lunch Options';
+    const resultsHeader = document.createElement('th');
+    resultsHeader.textContent = 'Total votes';
+    headerRow.appendChild(optionNameHeader);
+    headerRow.appendChild(resultsHeader);
+    tableHeader.appendChild(headerRow);
+    table.appendChild(tableHeader);
+
+    // set table body
+    const tableBody = document.createElement('tbody');
+
+    // loop thru and fill table with data
+    for (let i = 0; i < optionsList.length; i++) {
+
+        // new row
+            const row = document.createElement('tr');
+        // cell for choice
+            const choiceCell = document.createElement('td');
+        // add choice radio button
+            let radioButton = document.createElement('input');
+            radioButton.setAttribute('type', 'radio');
+            radioButton.setAttribute('id', `Options${i+1}`);
+            radioButton.setAttribute('name', 'choice');
+            radioButton.setAttribute('value', optionsList[i].choice);
+        // label radio button
+            let label = document.createElement('label');
+            label.setAttribute('for', optionsList[i].choice);
+            label.textContent = optionsList[i].choice;
+        // append to option cell
+            choiceCell.appendChild(radioButton);
+            choiceCell.appendChild(label);
+        // append to row
+            row.appendChild(choiceCell);
+        // create result cell
+            const resultCell = document.createElement('td');
+            resultCell.setAttribute('id', `ResultsForOption${i+1}`);
+            resultCell.textContent = optionsList[i].voteCount;
+        // append to row
+            row.appendChild(resultCell);
+        // add row to the table
+        tableBody.appendChild(row);
+    }
+    // append body to table
+    table.appendChild(tableBody);
+
+    // append table to the DIV
+    voteAndResultsHolder.appendChild(table);
+}
+
+
+// On load
+window.onload = async function() {
+
+    // fetch poll options
+    const todaysOptions = await pollOptions()
+
+    //  create table and fill it
+    lunchOptiosnTableConstructor(todaysOptions);
+}
